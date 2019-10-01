4840a0d759cc4e764b73f474a3931566bfb1f196
alexaddros <46838438+alexaddros@users.noreply.github.com>
2019-10-01 17:39:16 +0300

    Update server.py

--- server.py
+++ server.py
@@ -3,14 +3,14 @@ import socket
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 sock.bind (('', 9090))
 
-client = [] # Массив где храним адреса клиентов
+client = [] # Массив, где храним адреса клиентов
 print ('Start Server')
 
 while True:
          data , addres = sock.recvfrom(1024)
          print (addres[0], addres[1])
          if  addres not in client : 
-                 client.append(addres)# Если такова клиента нету , то добавить
+                 client.append(addres) # Если такого клиента нету , то добавить
          for clients in client :
                  if clients == addres: 
                      continue # Не отправлять данные клиенту который их прислал
