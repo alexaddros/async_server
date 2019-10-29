56471cb25bd9e8b7c3fe3eb9e484d9e747682937
alexaddros <root@skopcovs1.fvds.ru>
2019-10-29 10:39:41 -0400

    modified:   server.py

--- server.py
+++ server.py
@@ -1,17 +1,19 @@
 import socket
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
-sock.bind (('', 9090))
+sock.bind (('skopcovs1.fvds.ru', 9090))
 
 client = [] # Массив, где храним адреса клиентов
-print ('Start Server')
+print ('Start Server\n')
 
 while True:
-         data , addres = sock.recvfrom(1024)
-         print (addres[0], addres[1])
-         if  addres not in client : 
-                 client.append(addres) # Если такого клиента нету , то добавить
-         for clients in client :
-                 if clients == addres: 
-                     continue # Не отправлять данные клиенту который их прислал
-                 sock.sendto(data,clients)
+        data , addres = sock.recvfrom(1024)
+        if  addres not in client : 
+                client.append(addres)
+                print("------------")
+                print("Nickname:", data.split()[0].decode("utf-8"), "\nIP:      ", addres[0])
+                print("------------\n")
+        for clients in client :
+                if clients == addres: 
+                    continue # Не отправлять данные клиенту который их прислал
+                sock.sendto(data,clients)	
