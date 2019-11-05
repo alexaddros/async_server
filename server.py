72b43178c30e9e23895b124373e140aa37cb62a0
alexaddros <46838438+alexaddros@users.noreply.github.com>
2019-11-05 17:46:13 +0300

    Update server.py

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import socket
 import authorization
 
 server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server.bind(('skopcovs1.fvds.ru', 9090))
+server.bind(('localhost', 9090))
 clients = []
 
 print('Start Server\n')
@@ -13,7 +13,7 @@ while True:
         data, address = server.recvfrom(1024)
         if data.decode().split("---_---")[-1] == "USERINFO":
                 clients.append(address)
-                nickname, password = data.decode().split("---_---")
+                nickname, password = data.decode().split("---_---")[:-1]
                 if nickname not in authorization.get_nicknames():
                     new_user_created = authorization.create_new(nickname,
                                                                 hashlib.sha1(password.encode('utf-8')).hexdigest())
@@ -26,6 +26,11 @@ while True:
                 else:
                     if authorization.authorize(nickname, hashlib.sha1(password.encode('utf-8')).hexdigest()):
                         server.sendto('SUCCESS'.encode('utf-8'), address)
+                        for client in clients:
+                            if client == address:
+                                continue
+                            server.sendto(f'------------ {nickname} connected to server ------------'.encode('utf-8'),
+                                          client)
         else:
             for client in clients:
                     if client == address:
